b3dcdbb5c77e4a334cae6537e338c29c9d932489
John Milller <staremperor@gmail.com>
2024-05-05 14:42:00 -0700

    Added simple login/logout

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,16 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "amplifyreactapp640d8e7e": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    },
+    "userPoolGroups": {
+      "amplifyreactappgroupGroupRole": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import awsconfig from './aws-exports';
+import {Amplify} from 'aws-amplify';
+import { withAuthenticator} from '@aws-amplify/ui-react';
 
-function App() {
+Amplify.configure(awsconfig);
+
+function App({signOut, user}) {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <button onClick={signOut}>Log Out</button>
+        <p>{user && user.signInDetails && user.signInDetails.loginId}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -22,4 +26,4 @@ function App() {
   );
 }
 
-export default App;
+export default withAuthenticator(App, true);
